8fa263df4025664f84b514f8981d61cd1feaebb4
NARONGPON.SU <narongpon.su@glsict.com>
2018-04-05 14:20:50 +0700

    add new Movie

--- src/components/AddMovie.js
+++ src/components/AddMovie.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react'
 
 class AddMovie extends Component {
+
+	addNewMovie(e) {
+		e.preventDefault()
+		let movie = {
+			title: this.title.value,
+			year: this.year.value,
+			description: this.description.value,
+			poster: this.poster.value
+		}
+
+		this.props.addMovie(movie)
+	}
+
 	render() {
 		return(
-			<form className="movie-form">
+			<form onSubmit={(e) => this.addNewMovie(e)} className="movie-form">
 				<p>Add a Movie</p>
 				<input ref={ (input) => this.title = input } type="text" placeholder="Title" />
 				<input ref={ (input) => this.year = input } type="text" placeholder="Year" />
 				<input ref={ (input) => this.poster = input } type="text" placeholder="Poster" />
 				<textarea ref={ (input) => this.description = input } type="text" placeholder="Description" ></textarea>
-				<button type="submit"> Add Movie</button>
+				<button type="submit">Add Movie</button>
 			</form>
 		)
 	}
--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import '../App.css';
 import Header from './Header'
 import Movie from './Movie'
+import AddMovie from './AddMovie'
 import {initialMovies} from '../movies'
 import {additionalMovies} from '../movies'
 
@@ -13,11 +14,22 @@ class App extends Component {
       movies: initialMovies
     }
     this.loadAdditionalMovies = this.loadAdditionalMovies.bind(this)
+    this.addMovieToGallery = this.addMovieToGallery.bind(this);
   }
 
   loadAdditionalMovies() {
     let currentMovies = {...this.state.movies}
     let newMovies = Object.assign(currentMovies, additionalMovies)
+    this.setState({ movies: newMovies })
+  }
+
+  addMovieToGallery(movie) {
+    console.log("mooooooooovie", movie)
+    let ts = Date.now()
+    let newMovie = {}
+    newMovie['movie' + ts] = movie
+    let currentMovies = { ...this.state.movies }
+    let newMovies = Object.assign(currentMovies, newMovie)
     this.setState({ movies: newMovies })
   }
 
@@ -34,6 +46,7 @@ class App extends Component {
           }
         </div>
         <div className="add-movies"><button onClick={this.loadAdditionalMovies}>Load more...</button></div>
+        <AddMovie addMovie={this.addMovieToGallery} />
       </div>
     );
   }
